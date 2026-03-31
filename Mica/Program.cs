db0deb471b33080ce187762cb14520b4f2526471
Ada Freya Ahmed <ada@chronovore.dev>
2026-04-01 02:04:14 +0530

    chore: initial commit

---
+++ Mica/Program.cs
@@ -0,0 +1,13 @@
+// SPDX-FileCopyrightText: 2026 Neptuwunium <ada@chronovore.dev>
+//
+// SPDX-License-Identifier: EUPL-1.2
+
+﻿namespace Mica;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+<!--
+SPDX-FileCopyrightText: 2026 Neptuwunium <ada@chronovore.dev>
+
+SPDX-License-Identifier: EUPL-1.2
+-->
+
+# Feldspar
+
+KTGL Again
