0ed9f19fc5d780cf35983bfeaa2a40bdd1f59cb0
Ada Freya Ahmed <ada@chronovore.dev>
2026-04-02 11:27:50 +0530

    chore: g1a, g1s

--- Feldspar/Package/ResourceMagic.cs
+++ Feldspar/Package/ResourceMagic.cs
@@ -10,16 +10,20 @@ namespace Feldspar.Package;
 [GenerateMagic]
 [Magic("G1TG", "Texture")]
 [Magic("G1M_", "Model")]
+[Magic("G1A_", "Animation")]
 [Magic("G2A_", "Animation2")]
 [Magic("SWGQ", "MaterialInstance", false)]
-[Magic("G2S_", "Shader2", false)]
+[Magic("G1S_", "Shader")]
+[Magic("G2S_", "Shader2")]
 public readonly partial record struct ResourceMagic {
 	public string Ext =>
 		Value switch {
 			Texture => ".g1t",
 			Model => ".g1m",
+			Animation => ".g1a",
 			Animation2 => ".g2a",
 			MaterialInstance => ".swg",
+			Shader	=> ".g1s",
 			Shader2	=> ".g2s",
 			_ => string.Empty,
 		};
