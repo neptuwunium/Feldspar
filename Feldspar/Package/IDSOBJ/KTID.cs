321a7b99e0bc7ab470d75ed492eea08cb785e159
Ada Freya Ahmed <ada@chronovore.dev>
2026-04-27 21:51:33 +0530

    pluto[sourcegen]: use concrete type

--- Feldspar/Package/IDSOBJ/KTID.cs
+++ Feldspar/Package/IDSOBJ/KTID.cs
@@ -3,4 +3,8 @@ using Pluto.SourceGen.TransparentStructGenerator;
 namespace Feldspar.Package.IDSOBJ;
 
 [TransparentStruct<uint>]
-public partial struct KTID;
+public partial struct KTID {
+	public override string ToString() {
+		return Value.ToString("x8");
+	}
+}
