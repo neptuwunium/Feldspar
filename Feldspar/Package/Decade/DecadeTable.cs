5580109ceadb7689503368bcd7e02c76e5f7de0a
Ada Freya Ahmed <ada@chronovore.dev>
2026-05-01 00:10:04 +0530

    decade: inlnie BlockCompression

--- Feldspar/Package/Decade/DecadeTable.cs
+++ Feldspar/Package/Decade/DecadeTable.cs
@@ -4,7 +4,7 @@
 
 using System.Diagnostics;
 using System.Text;
-using Feldspar.Compression;
+using Charon.Compression;
 using Feldspar.Package.Format.Decade;
 using Feldspar.Security;
 using Pluto.Extensions;
@@ -19,7 +19,7 @@ public class DecadeTable {
 		using var stream = new ArrayPoolBinaryReader(data, leaveOpen);
 		var length = stream.Read<int>();
 		DecadeCipher.Crypt(data.Span[4..], length, DecadeKeyRing.Decade);
-		using var decompressed = BlockCompression.Decompress(stream, length);
+		using var decompressed = Decompress(stream, length);
 		using var tableReader = new ArrayPoolBinaryReader(decompressed, leaveOpen);
 
 		var header = tableReader.Read<DecadeTableHeader>();
@@ -61,7 +61,7 @@ public class DecadeTable {
 			}
 
 			try {
-				return BlockCompression.Decompress(block, lookup.Info.MemorySize);
+				return Decompress(block, lookup.Info.MemorySize);
 			} finally {
 				block.Dispose();
 			}
@@ -69,5 +69,39 @@ public class DecadeTable {
 			block.Dispose();
 			throw;
 		}
+	}
+
+	public static RentedArray<byte> Decompress(BufferBinaryReader reader, int length) {
+		var result = new RentedArray<byte>(length);
+
+		var baseOffset = reader.Position;
+		var decPos = 0;
+		var decMem = result.Memory;
+		var decSpan = result.Span;
+
+		while (decPos < length) {
+			var size = reader.Read<int>();
+			var isCompressed = ((size >> 15) & 1) == 1;
+			size &= 0x7fff;
+
+			var start = reader.Position - baseOffset;
+
+			if (!isCompressed) {
+				reader.ReadBytes(decSpan.Slice(decPos, Math.Min(size, reader.Length - reader.Position)));
+				decPos += size;
+			} else {
+				using var shared = reader.ReadSharedBytes(size);
+				decPos += CompressionHelper.Decompress(CompressionType.Zlib, shared.Memory, decMem.Slice(decPos, Math.Min(0x4000, length - decPos)));
+			}
+
+			reader.Position = (start + size).Align(0x10) + baseOffset;
+		}
+
+		return result;
+	}
+
+	public static RentedArray<byte> Decompress(RentedArray<byte> data, int length) {
+		using var reader = new ArrayPoolBinaryReader(data);
+		return Decompress(reader, length);
 	}
 }
